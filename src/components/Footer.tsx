6b1e9ccf14da7dde0d19a9db0460edebedddee29
Wanja Herkt <w.herkt@tu-braunschweig.de>
2025-07-27 10:15:47 +0200

    change spacing to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-50 dark:bg-gray-900 border-t border-gray-200 dark:border-gray-600 mt-24">
+    <footer className="bg-gray-50 dark:bg-gray-900 border-t border-gray-200 dark:border-gray-600 mt-12">
       <div className="mx-auto max-w-content px-4 sm:px-6 lg:px-8 py-16">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
           {/* Kontakt */}
